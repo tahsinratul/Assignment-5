a4eb4e8458eb70d73e62520bf247f381deb3f752
tahsinratul <ratulprofessional123@gmail.com>
2025-03-02 23:32:00 +0600

    done

--- js/complete.js
+++ js/complete.js
@@ -7,11 +7,10 @@ document.getElementById('1st').addEventListener('click', function (event) {
     if (IntTaskText !== 0) {
         alert('Board Updated Successfully');
 
-        // Adding Task Text
+        
         const newTaskText = IntTaskText - 1;
         document.getElementById('task').innerText = newTaskText;
 
-        // Adding Nav Text
         const navText = document.getElementById('nav').innerText
         const IntNavText = parseInt(navText);
 
@@ -23,7 +22,6 @@ document.getElementById('1st').addEventListener('click', function (event) {
         firstCardBtn.disabled = true;
         firstCardBtn.style.backgroundColor = 'slategray';
 
-        // Push into Activity Div
         const heading1 = document.getElementById('hd1')
 
         const activity = document.getElementById('clear1');
@@ -38,3 +36,198 @@ document.getElementById('1st').addEventListener('click', function (event) {
         alert('Congrats!!!You have completed all current tasks')
     }
 })
+
+document.getElementById('2nd').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const taskText = document.getElementById('task').innerText
+    const IntTaskText = parseInt(taskText);
+
+    if (IntTaskText !== 0) {
+        alert('Board Updated Successfully');
+
+       
+        const newTaskText = IntTaskText - 1;
+        document.getElementById('task').innerText = newTaskText;
+
+        const navText = document.getElementById('nav').innerText
+        const IntNavText = parseInt(navText);
+
+        const newNavText = IntNavText + 1;
+
+        document.getElementById('nav').innerText = newNavText;
+
+        const firstCardBtn = document.getElementById('2nd');
+        firstCardBtn.disabled = true;
+        firstCardBtn.style.backgroundColor = 'slategray';
+
+        const heading1 = document.getElementById('hd2')
+
+        const activity = document.getElementById('clear1');
+
+        const div = document.createElement('div');
+        div.innerHTML = `<p class = "mt-2">You have completed the task ${heading1.innerText} at ${
+          new Date().toLocaleTimeString()
+        }</p>`
+
+        activity.appendChild(div)
+    } else {
+        alert('Congrats!!!You have completed all current tasks')
+    }
+})
+
+document.getElementById('3rd').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const taskText = document.getElementById('task').innerText
+    const IntTaskText = parseInt(taskText);
+
+    if (IntTaskText !== 0) {
+        alert('Board Updated Successfully');
+
+        const newTaskText = IntTaskText - 1;
+        document.getElementById('task').innerText = newTaskText;
+
+        const navText = document.getElementById('nav').innerText
+        const IntNavText = parseInt(navText);
+
+        const newNavText = IntNavText + 1;
+
+        document.getElementById('nav').innerText = newNavText;
+
+        const firstCardBtn = document.getElementById('3rd');
+        firstCardBtn.disabled = true;
+        firstCardBtn.style.backgroundColor = 'slategray';
+
+        const heading1 = document.getElementById('hd3')
+
+        const activity = document.getElementById('clear1');
+
+        const div = document.createElement('div');
+        div.innerHTML = `<p class = "mt-2">You have completed the task ${heading1.innerText} at ${
+          new Date().toLocaleTimeString()
+        }</p>`
+
+        activity.appendChild(div)
+    } else {
+        alert('Congrats!!!You have completed all current tasks')
+    }
+})
+
+document.getElementById('4th').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const taskText = document.getElementById('task').innerText
+    const IntTaskText = parseInt(taskText);
+
+    if (IntTaskText !== 0) {
+        alert('Board Updated Successfully');
+
+        // Adding Task Text
+        const newTaskText = IntTaskText - 1;
+        document.getElementById('task').innerText = newTaskText;
+
+        const navText = document.getElementById('nav').innerText
+        const IntNavText = parseInt(navText);
+
+        const newNavText = IntNavText + 1;
+
+        document.getElementById('nav').innerText = newNavText;
+
+        const firstCardBtn = document.getElementById('4th');
+        firstCardBtn.disabled = true;
+        firstCardBtn.style.backgroundColor = 'slategray';
+
+        const heading1 = document.getElementById('hd4')
+
+        const activity = document.getElementById('clear1');
+
+        const div = document.createElement('div');
+        div.innerHTML = `<p class = "mt-2">You have completed the task ${heading1.innerText} at ${
+          new Date().toLocaleTimeString()
+        }</p>`
+
+        activity.appendChild(div)
+    } else {
+        alert('Congrats!!!You have completed all current tasks')
+    }
+})
+
+document.getElementById('5th').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const taskText = document.getElementById('task').innerText
+    const IntTaskText = parseInt(taskText);
+
+    if (IntTaskText !== 0) {
+        alert('Board Updated Successfully');
+
+        // Adding Task Text
+        const newTaskText = IntTaskText - 1;
+        document.getElementById('task').innerText = newTaskText;
+
+        const navText = document.getElementById('nav').innerText
+        const IntNavText = parseInt(navText);
+
+        const newNavText = IntNavText + 1;
+
+        document.getElementById('nav').innerText = newNavText;
+
+        const firstCardBtn = document.getElementById('5th');
+        firstCardBtn.disabled = true;
+        firstCardBtn.style.backgroundColor = 'slategray';
+
+        const heading1 = document.getElementById('hd5')
+
+        const activity = document.getElementById('clear1');
+
+        const div = document.createElement('div');
+        div.innerHTML = `<p class = "mt-2">You have completed the task ${heading1.innerText} at ${
+          new Date().toLocaleTimeString()
+        }</p>`
+
+        activity.appendChild(div)
+    } else {
+        alert('Congrats!!!You have completed all current tasks')
+    }
+})
+
+document.getElementById('6th').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const taskText = document.getElementById('task').innerText
+    const IntTaskText = parseInt(taskText);
+
+    if (IntTaskText !== 0) {
+        alert('Board Updated Successfully');
+
+        // Adding Task Text
+        const newTaskText = IntTaskText - 1;
+        document.getElementById('task').innerText = newTaskText;
+
+        const navText = document.getElementById('nav').innerText
+        const IntNavText = parseInt(navText);
+
+        const newNavText = IntNavText + 1;
+
+        document.getElementById('nav').innerText = newNavText;
+
+        const firstCardBtn = document.getElementById('6th');
+        firstCardBtn.disabled = true;
+        firstCardBtn.style.backgroundColor = 'slategray';
+
+        const heading1 = document.getElementById('hd6')
+
+        const activity = document.getElementById('clear1');
+
+        const div = document.createElement('div');
+        div.innerHTML = `<p class = "mt-2">You have completed the task ${heading1.innerText} at ${
+          new Date().toLocaleTimeString()
+        }</p>`
+
+        activity.appendChild(div)
+    } else {
+        alert('Congrats!!!You have completed all current tasks')
+    }
+})
+
